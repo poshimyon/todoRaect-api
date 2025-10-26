b574f2b85ad618a3eb73e1e6ee27be45fe880e14
takumi.iihoshi <takumi75.poshi@gmail.com>
2025-10-26 13:21:02 +0900

    local-dynamo環境構築

--- src/client.ts
+++ src/client.ts
@@ -2,8 +2,12 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-    region: "ap-northeast-1",
+    region: "local", // 👈 ← これが重要（または削除でもOK）
     endpoint: "http://localhost:8000",
+    credentials: {
+        accessKeyId: "fakeMyKeyId",
+        secretAccessKey: "fakeSecretAccessKey",
+    },
 });
 
 export const ddb = DynamoDBDocumentClient.from(client);
