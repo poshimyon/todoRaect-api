3ef918f4baad033d2f7cd3c9314bcac1e8901dfb
t-masuda-ctof <taka1dptx@gmail.com>
2025-10-26 19:03:27 +0900

    localに固定

--- src/client.ts
+++ src/client.ts
@@ -2,15 +2,13 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-    region: "local", // 👈 ← これが重要（または削除でもOK）
-    endpoint:
-        process.platform === "win32"
-            ? "http://host.docker.internal:8000"
-            : "http://localhost:8000",
-    credentials: {
-        accessKeyId: "fakeMyKeyId",
-        secretAccessKey: "fakeSecretAccessKey",
-    },
+  region: "local",
+  endpoint: "http://localhost:8000",
+  credentials: {
+    accessKeyId: "fakeMyKeyId",
+    secretAccessKey: "fakeSecretAccessKey",
+  },
 });
+
 
 export const ddb = DynamoDBDocumentClient.from(client);
